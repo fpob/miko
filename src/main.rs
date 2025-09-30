98d0e84f35cb69f63c632140e1f06cc8d9e88034
Filip Pobořil <dev@fpob.cz>
2025-09-30 18:50:21 +0200

    Rename `gen` variable
    `gen` is a reserved keyword in 2024 edition.

--- src/main.rs
+++ src/main.rs
@@ -130,8 +130,13 @@ const FILE_KEYS: &[&str] = &[
     "gsname",
 ];
 
-fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
-    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+fn print_completions<G: Generator>(generator: G, cmd: &mut Command) {
+    generate(
+        generator,
+        cmd,
+        cmd.get_name().to_string(),
+        &mut io::stdout(),
+    );
 }
 
 fn main() -> anyhow::Result<()> {
